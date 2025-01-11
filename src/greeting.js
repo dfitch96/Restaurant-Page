26f8e96be14230fba16437063675a1f5e50f3b3f
Dylan Fitch <kkarl30@gmail.com>
2025-01-11 14:32:17 -0500

    installed packages and configured webpack

---
+++ src/greeting.js
@@ -0,0 +1,7 @@
+export const greeting = "Hello World";
+import batman from "./images/4043232_avatar_batman_comics_hero_icon.png";
+
+const image = document.createElement("img");
+image.src = batman;
+
+document.body.appendChild(image);
