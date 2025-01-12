1e06b1fb9077e5e5e4c1d06a6f9a62c0fad18b75
Dylan Fitch <kkarl30@gmail.com>
2025-01-11 22:34:33 -0500

    completed header

--- src/greeting.js
+++
@@ -1,7 +0,0 @@
-export const greeting = "Hello World";
-import batman from "./images/4043232_avatar_batman_comics_hero_icon.png";
-
-const image = document.createElement("img");
-image.src = batman;
-
-document.body.appendChild(image);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 
-import {greeting} from "./greeting.js";
 import "./styles.css";
-console.log(greeting);
